c77a11112b9fe01ceda5676c9c878cdda704b720
Manjupatat <109718621+Manjupatat@users.noreply.github.com>
2022-11-21 10:52:33 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,7 +5,7 @@ public class BubbleSortExample {
          for(int i=0; i < n; i++){  
                  for(int j=1; j < (n-i); j++){  
                           if(arr[j-1] > arr[j]){  
-                                 //swap elements  
+                                 //swap elements the given elements 
                                  temp = arr[j-1];  
                                  arr[j-1] = arr[j];  
                                  arr[j] = temp;  
